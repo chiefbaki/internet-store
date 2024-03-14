a5f3d8a0f3116361ec853e8541f5d63412a918d2
Unity01 <61545789+chiefbaki@users.noreply.github.com>
2024-03-14 23:31:49 +0600

    Initial commit

---
+++ src/test/java/kg/islam/application/internet/store/InternetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.islam.application.internet.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
